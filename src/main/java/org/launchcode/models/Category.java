1b55069e41fdc9b1f975818c69e0743536abb319
Courtney Hasenstab <hasenstab.courtney@gmail.com>
2019-04-14 13:14:30 -0500

    yuuup

--- src/main/java/org/launchcode/models/Category.java
+++ src/main/java/org/launchcode/models/Category.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @Entity
 public class Category {
+
     @Id
     @GeneratedValue
     private int id;
@@ -16,12 +17,18 @@ public class Category {
     @Size(min=3, max=15)
     private String name;
 
-    public Category(){
+    @OneToMany
+    @JoinColumn(name = "category_id")
+    private List<Cheese> cheeses = new ArrayList<>();
 
+    public Category() {}
+
+    public Category(String name){
+        this.name=name;
     }
 
-    public Category(String Name){
-        name=Name;
+    public int getId() {
+        return id;
     }
 
     public String getName() {
@@ -30,24 +37,5 @@ public class Category {
 
     public void setName(String name) {
         this.name = name;
-    }
-    @OneToMany
-    @JoinColumn(name = "category_id")
-    private List<Cheese> cheeses = new ArrayList<>();
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public List<Cheese> getCheeses() {
-        return cheeses;
-    }
-
-    public void setCheeses(List<Cheese> cheeses) {
-        this.cheeses = cheeses;
     }
 }
